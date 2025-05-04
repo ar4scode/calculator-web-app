7e71d1da278ebe871398c7bf540b70b0b8057e35
Ar4sCode <ar4sazizi@gmail.com>
2025-05-05 01:27:46 +0330

    Add operators and fix adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,33 @@ function App() {
   return (
     <>
       <div className='bg-white rounded-2xl shadow-2xl p-4'>
-        <div className='shadow-md rounded-2xl bg-gray-100 flex justify-end text-5xl text-gray-400 px-5 py-2 mb-4'>
+        <div className='shadow-md rounded-2xl bg-gray-100 flex justify-end text-5xl text-gray-400 px-5 py-2 my-8'>
           0
         </div>
-        <div className='grid grid-cols-3 gap-5 bg-white p-4'>
-          <NumberButton number={7}/>
-          <NumberButton number={8}/>
-          <NumberButton number={9}/>
-          <NumberButton number={4}/>
-          <NumberButton number={5}/>
-          <NumberButton number={6}/>
-          <NumberButton number={1}/>
-          <NumberButton number={2}/>
-          <NumberButton number={3}/>
-          <NumberButton number={0}/>
+        <div className='grid grid-cols-4 gap-4 bg-white p-2 md:p-4 md:gap-6'>
+          <NumberButton label="clr"/>
+          <NumberButton label="Del"/>
+          <NumberButton label="%"/>
+          <NumberButton label="/"/>
+
+          <NumberButton label={7}/>
+          <NumberButton label={8}/>
+          <NumberButton label={9}/>
+          <NumberButton label="*"/>
+
+          <NumberButton label={4}/>
+          <NumberButton label={5}/>
+          <NumberButton label={6}/>
+          <NumberButton label="-"/>
+
+          <NumberButton label={1}/>
+          <NumberButton label={2}/>
+          <NumberButton label={3}/>
+          <NumberButton label="+"/>
+
+          <NumberButton label={0}/>
+          <NumberButton label="."/>
+          <NumberButton label="="/>
         </div>
       </div>
     </>
--- src/components/NumberButton.tsx
+++ src/components/NumberButton.tsx
@@ -1,17 +1,19 @@
 import { Button } from "@mui/material"
 
-type NumberButtonProps = {
-  number : number;
+type Props = {
+  label : string | number;
+  className ?: string // optional : you can use this prop or not
 }
 
-const NumberButton = ({number} : NumberButtonProps) => {
+const NumberButton = ({label, className} : Props) => {
   return (
-    <Button 
+    <Button
+      className={className} 
       variant="contained"
       size="large"
-      style={{background: "#f0e9e9", color: "#000", borderRadius: "35px", padding: "15px 0", fontWeight: "bold"}}
+      style={{background: "#f0e9e9", color: "#000", borderRadius: "30px", padding: "15px 0", fontWeight: "bold"}}
     >
-      {number}
+      {label}
     </Button>
   ) 
 }
